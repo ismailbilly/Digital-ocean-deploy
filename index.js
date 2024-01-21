67c88b70bb1389b3ea38924acccbadc3a6f25b20
Billy <ismail.bilkisu@gmail.com>
2024-01-21 18:05:22 +0100

    server test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const app = express();
-
+app.get('/', (req, res) => {
+    res.send("I am hosted")
+})
 app.listen(8912, () => {
   console.log("====================================");
   console.log("server is running on http:localhost:8912");
